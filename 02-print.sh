0bec5a5c820b3c61f67446a5b77513b1b13949ec
kavya <vijayakavya1226@gmail.com>
2023-09-10 15:22:48 +0530

    expense shell

--- 02-print.sh
+++ 02-print.sh
@@ -1,8 +1,8 @@
 echo Hello World
 
 echo -e "\e[31mRED COLOR\e[0m"
-echo -e "\e[31mGREEN COLOR\e[0m"
-echo -e "\e[31mYELLOW COLOR\e[0m"
-echo -e "\e[31mBLUE COLOR\e[0m"
-echo -e "\e[31mMAGENTA COLOR\e[0m"
-echo -e "\e[31mCYAN COLOR\e[0m"
+echo -e "\e[32mGREEN COLOR\e[0m"
+echo -e "\e[33mYELLOW COLOR\e[0m"
+echo -e "\e[34mBLUE COLOR\e[0m"
+echo -e "\e[35mMAGENTA COLOR\e[0m"
+echo -e "\e[36mCYAN COLOR\e[0m"
