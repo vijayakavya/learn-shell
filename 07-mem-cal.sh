e0825687fc40d25a9aa43e87c998855f87af461d
kavya <vijayakavya1226@gmail.com>
2023-09-13 16:21:23 +0530

    expense shell

--- 07-mem-cal.sh
+++ 07-mem-cal.sh
@@ -1,5 +1,5 @@
 total=$(free | grep Mem | awk '{print $2}')
 free=$(free | grep mem | awk '{print 44}')
 
-free_percentage=$echo "$free / $total * 100" | bc)
+free_percentage=$echo "$free / $total * 100" | bc -l | awk -F . '{print $1}')
 echo $free_percentage
